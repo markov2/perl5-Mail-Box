fb807761d1ee7def66f3f74e84fc2dd161e2970a
Mark Overmeer <mark@overmeer.net>
2001-09-19 13:14:44 +0200

    distribution Mail-Box-2.0011.tar.gz

---
+++ Mail-Box-2.00_11/Mail/Message/Body/Lines.pm
@@ -0,0 +1,203 @@
+use strict;
+use warnings;
+
+package Mail::Message::Body::Lines;
+use base 'Mail::Message::Body';
+
+use Mail::Box::Parser;
+use IO::Lines;
+
+our $VERSION = '2.00_11';
+
+use Carp;
+
+=head1 NAME
+
+ Mail::Message::Body::Lines - Body of a Mail::Message stored as array of lines
+
+=head1 CLASS HIERARCHY
+
+ Mail::Message::Body::Lines
+ is a Mail::Message::Body
+ is a Mail::Reporter
+
+=head1 SYNOPSIS
+
+ See Mail::Message::Body
+
+=head1 DESCRIPTION
+
+READ C<Mail::Message::Body> FIRST. This manual-page only describes the
+extensions to the default body functionality.
+
+The body (content) of a message can be stored in various ways.  In this
+documentation you find the description of extra functionality you have
+when a message is stored in an array of lines.
+
+Storing a whole message as an array of lines is useful when the data is not
+encoded, and you want to process it on a line-by-line basis (a common practice
+for inspecting message bodies).
+
+=head1 METHOD INDEX
+
+The general methods for C<Mail::Message::Body::Lines> objects:
+
+  MMB data FILE | LIST-OF-LINES |...   MMB nrLines
+   MR errors                               print [FILE]
+  MMB file                             MMB reply OPTIONS
+  MMB isDelayed                         MR report [LEVEL]
+  MMB isMultipart                       MR reportAll [LEVEL]
+  MMB lines                            MMB size
+   MR log [LEVEL [,STRINGS]]           MMB string
+  MMB message [MESSAGE]                MMB stripSignature OPTIONS
+  MMB modified [BOOL]                   MR trace [LEVEL]
+      new OPTIONS                       MR warnings
+
+The extra methods for extension writers:
+
+   MR DESTROY                           MR logPriority LEVEL
+  MMB clone                             MR logSettings
+   MR inGlobalDestruction               MR notImplemented
+  MMB load                             MMB read PARSER, HEAD, BODYTYPE...
+
+Methods prefixed with an abbreviation are described in the following
+manual-pages:
+
+   MR = L<Mail::Reporter>
+  MMB = L<Mail::Message::Body>
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+#------------------------------------------
+
+=item new OPTIONS
+
+ OPTION    DESCRIBED IN                  DEFAULT
+ data      Mail::Message::Body           undef
+ log       Mail::Reporter                'WARNINGS'
+ message   Mail::Message::Body           undef
+ modified  Mail::Message::Body           0
+ trace     Mail::Reporter                'WARNINGS'
+
+=cut
+
+#------------------------------------------
+
+sub string() { join '', @{shift->{MMBL_array}} }
+
+#------------------------------------------
+
+sub lines()
+{   wantarray ? @{shift->{MMBL_array}}   # all lines
+      : [ @{shift->{MMBL_array}} ]       # new ref array to avoid accidental
+}                                        #   destruction of body itself.
+
+#------------------------------------------
+
+sub file() { IO::Lines->new(shift->{MMBL_array}) }
+
+#------------------------------------------
+
+sub nrLines() { scalar @{shift->{MMBL_array}} }
+
+#------------------------------------------
+# Optimized to be computed only once.
+
+sub size()
+{   my $self = shift;
+    return $self->{MMBL_size} if exists $self->{MMBL_size};
+
+    my $size = 0;
+    $size += length $_ foreach @{$self->{MMBL_array}};
+    $self->{MMBL_size} = $size;
+}
+
+#------------------------------------------
+
+=item print [FILE]
+
+=cut
+
+sub print(;$)
+{   my $self = shift;
+    my $fh   = shift || \*STDOUT;
+    $fh->print(@{$self->{MMBL_array}});
+}
+
+#------------------------------------------
+
+=back
+
+=head1 METHODS for extension writers
+
+=over 4
+
+=cut
+
+#------------------------------------------
+
+sub data(@)
+{   my $self = shift;
+    delete $self->{MMBL_size};
+    $self->SUPER::data(@_);
+}
+
+sub _data_from_file(@_)
+{   my ($self, $fh) = @_;
+    my @data = ref $fh eq 'GLOB' ? <$fh> : $fh->getlines;
+    $self->{MMBL_array} = \@data;
+}
+
+sub _data_from_lines(@_)
+{   my ($self, $lines)  = @_;
+    $lines = [ split /(?<=\n)/, $lines->[0] ] # body passed in one string.
+        if @$lines==1;
+
+    $self->{MMBL_array} = $lines;
+}
+
+#------------------------------------------
+
+sub read($$;$@)
+{   my ($self, $parser, $head, $bodytype) = splice @_, 0, 4;
+    $self->{MMBL_array} = [ $parser->bodyAsList(@_) ];
+    $self->{MMB_where} = shift @{$self->{MMBL_array}};
+    $self;
+}
+
+#------------------------------------------
+
+sub clone()
+{   my $self  = shift;
+    ref($self)->new(data => [ $self->lines ] );
+}
+
+#------------------------------------------
+
+=back
+
+=head1 SEE ALSO
+
+L<Mail::Box-Overview>
+
+=head1 AUTHOR
+
+Mark Overmeer (F<mailbox@overmeer.net>).
+All rights reserved.  This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=head1 VERSION
+
+This code is beta, version 2.00_11.
+
+Copyright (c) 2001 Mark Overmeer. All rights reserved.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Mail-Box-2.00_11/Mail/Message/Part.pm
@@ -0,0 +1,138 @@
+use strict;
+use warnings;
+
+package Mail::Message::Part;
+use base 'Mail::Message';
+
+our $VERSION = '2.00_11';
+
+=head1 NAME
+
+Mail::Message::Part - a part of a Mail::Message, but a message by itself.
+
+=head1 CLASS HIERARCHY
+
+ Mail::Message::Part
+ is a Mail::Message
+ is a Mail::Reporter
+
+=head1 SYNOPSIS
+
+ my Mail::Message $message = ...;
+ if($message->isMultipart) {
+     my Mail::Message::Part $part;
+
+     foreach $part ($message->body->parts) {
+         $part->print(\*STDOUT);
+         my $attachbody = $part->head;
+         my $attachhead = $part->body;
+     }
+ }
+
+=head1 DESCRIPTION
+
+A C<Mail::Message::Part> object contains a message which is included in
+an other message.  For instance I<attachments> are I<parts>.
+
+READ C<Mail::Message> FIRST.  A part is a special message: it has a
+reference to its parent message, and will usually not be sub-classed
+into mail-folder-specific variants.
+
+=head1 METHOD INDEX
+
+The general methods for C<Mail::Message::Part> objects:
+
+   MM attach MESSAGES, OPTIONS          MM modified [BOOL]
+   MM decoded OPTIONS                      new OPTIONS
+   MM encode TYPE, OPTIONS              MM nrLines
+   MR errors                            MM parent
+   MM get FIELD                         MM print [FILEHANDLE]
+   MM guessTimestamp                    MR report [LEVEL]
+   MM isDummy                           MR reportAll [LEVEL]
+   MM isMultipart                       MM size
+   MM isPart                            MM timestamp
+   MR log [LEVEL [,STRINGS]]            MM toplevel
+   MM messageId                         MR trace [LEVEL]
+
+The extra methods for extension writers:
+
+   MM DESTROY                           MR logPriority LEVEL
+   MM body [BODY, OPTIONS]              MR logSettings
+   MM clone                             MR notImplemented
+   MM coerce MESSAGE [,OPTIONS]         MM read PARSER, [BODYTYPE]
+   MM head [HEAD]                       MM readBody PARSER, HEAD [, BO...
+   MR inGlobalDestruction               MM readHead PARSER [,CLASS]
+   MM isDelayed                         MM storeBody BODY
+
+Methods prefixed with an abbreviation are described in the following
+manual-pages:
+
+   MM = L<Mail::Message>
+   MR = L<Mail::Reporter>
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+#------------------------------------------
+
+=item new OPTIONS
+
+Create a message part.  You can add the following options:
+
+ OPTIONS              DESCRIBED IN           DEFAULT
+ body                 Mail::Message          <undef>
+ head                 Mail::Message          <undef>
+ parent               Mail::Message::Part    <obligatory>
+
+=over 4
+
+=item * parent =E<gt> MESSAGE
+
+(obligatory) reference to the parental C<Mail::Message> object where this
+part is a member of.  That object may be a part itself.
+
+=back
+
+sub init($)
+{   my ($self, $args) = @_;
+    $self->SUPER::init($args);
+
+    $self->{MM_parent} = $args{parent}
+        or confess "No parent specified for part.\n";
+
+    $self;
+}
+
+sub parent()     { shift->{MM_parent} }            # overrides
+sub toplevel()   { shift->{MM_parent}->toplevel }  # idem
+sub isPart()     { 1 }                             # idem
+
+#------------------------------------------
+
+
+=back
+
+=head1 SEE ALSO
+
+L<Mail::Box-Overview>
+
+=head1 AUTHOR
+
+Mark Overmeer (F<mailbox@overmeer.net>).
+All rights reserved.  This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=head1 VERSION
+
+This code is beta, version 2.00_11.
+
+Copyright (c) 2001 Mark Overmeer. All rights reserved.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
